f0674e1a7052c2818f37154c835c820f9525b72b
espanderlof <jaime717@hotmail.es>
2024-11-02 16:31:17 -0300

    CSP: script-src unsafe-inline - OWASP_2021_A05

--- web_recetas/src/main/java/com/demo/web_recetas/config/WebSecurityConfig.java
+++ web_recetas/src/main/java/com/demo/web_recetas/config/WebSecurityConfig.java
@@ -7,10 +7,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder; 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity; 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
 import com.demo.web_recetas.integration.CustomAuthenticationProvider;
-import org.springframework.security.crypto.password.PasswordEncoder; 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
  
@@ -20,7 +21,6 @@ public class WebSecurityConfig {
 
     @Autowired 
     private CustomAuthenticationProvider authProvider; 
- 
 
     @Bean 
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception { 
@@ -58,23 +58,32 @@ public class WebSecurityConfig {
             .headers(headers -> headers
                 .frameOptions(frame -> frame.deny())
                 .contentSecurityPolicy(csp -> csp
-                    .policyDirectives("default-src 'self'; " +
-                        "script-src 'self' 'unsafe-inline'; " +
-                        "style-src 'self' 'unsafe-inline'; " +
+                    .policyDirectives(
+                        "default-src 'self'; " +
+                        "script-src 'self'; " +
+                        "style-src 'self'; " +
                         "img-src 'self' data:; " +
                         "font-src 'self'; " +
                         "form-action 'self'; " +
                         "frame-ancestors 'none'")
                 )
             )
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/login", "/logout"));
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            );
 
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .requestMatchers(
+                "/css/**",
+                "/js/**",
+                "/images/**",
+                "/favicon.ico"
+            );
     }
 
     @Bean
